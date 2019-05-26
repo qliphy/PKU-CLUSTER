ce9c6dc3fa7f3da4260debf9806964724b4f8b45
Ying An <anying@pku.edu.cn>
2019-05-27 00:58:32 +0800

    remove events codes

--- RunII2017/MakeTemplate/pesudo/selectPhoton/remove-electrons/MakeTemplate.C
+++ RunII2017/MakeTemplate/pesudo/selectPhoton/remove-electrons/MakeTemplate.C
@@ -19,8 +19,10 @@ void MakeTemplate::Loop(TString name)
    if (fChain == 0) return;
    Long64_t nentries = fChain->GetEntriesFast();
 
-   TFile *newfile = new TFile( "./out"+name+ "_fakephoton.root", "recreate"); 
-   TTree *newtree = fChain->CloneTree(0);
+   TFile *newfile2 = new TFile( "./"+name+ "_fakeele.root", "recreate"); 
+   TTree *newtree2 = fChain->CloneTree(0);
+   Int_t position;
+   newtree2->Branch("position",&position,"position/I");
 //   nentries = 100000;
    int cut0=0,cut2=0,cut3=0,cut4=0;
    Long64_t nbytes = 0, nb = 0;
@@ -31,11 +33,19 @@ void MakeTemplate::Loop(TString name)
       cut0++;
       if(jentry%1000000==0)  cout<<jentry<<" ; "<<nentries<<endl;
       Int_t cut1=0;
+      int index[6] = {-1};
       for(Int_t j=0;j<6;j++){
-	      if (photon_pevnew[j]==1){
+	      if (photon_isprompt[j]==1&&photon_pevnew[j]==1){
 		      cut1++;
+                      index[j] = j;
 		      continue;
 	      }
+      }
+      for(Int_t i = 0; i<6; i++){
+         if(index[i]>-1){ 
+		 position = index[i];
+		 break;
+	 }
       }
       if (cut1!=0)
 	      cout<<"jentry = "<<jentry<<"  "<<"there is"<<" "<<cut1<<" photon(s) in the array"<<endl;
@@ -43,11 +53,12 @@ void MakeTemplate::Loop(TString name)
 	      continue;
       cut2++;
 
-      newtree->Fill();
+      newtree2->Fill();
+      
    }
-   newfile->cd();
-   newtree->Write(); //fill this tree into KEY from OBJ
-   newfile->Close();
+   newfile2->cd();
+   newtree2->Write(); //fill this tree into KEY from OBJ
+   newfile2->Close();
    //print some information
    std::cout << "The total events are : " << cut0 << std::endl;
    std::cout << "the selected events   : " << cut2 << std::endl;
---
+++ RunII2017/MakeTemplate/pesudo/selectPhoton/remove-electrons/hsito.C
@@ -0,0 +1,42 @@
+#define hsito_cxx
+#include "hsito.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#define ptnumber 6
+
+void hsito::Loop()
+{
+   if (fChain == 0) return;
+   Double_t lowpt[ptnumber] ={20,25,30,40,50,65};
+   Double_t highpt[ptnumber]={25,30,40,50,65,400};
+   TH1D* h1[ptnumber]; 
+   TCanvas*c1[ptnumber];
+   Long64_t nentries = fChain->GetEntriesFast();
+   Long64_t nbytes = 0, nb = 0;
+   for(int i =0;i<ptnumber;i++){
+   h1[i] = new TH1D(Form("h1_%i",i+1),Form("pt%0.f_%0.f",lowpt[i],highpt[i]),100,lowpt[i],highpt[i]);
+   c1[i] = new TCanvas(Form("c1_%i",i),Form("pt%0.f_%0.f",lowpt[i],highpt[i]),900,600);
+   h1[i]->Sumw2();
+   }
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+      for(Int_t i=0;i<ptnumber;i++){    
+        if(photon_pt[position]>lowpt[i]&&photon_pt[position]<highpt[i])
+		h1[i]->Fill(photon_pt[position],scalef);
+      }
+      
+  }
+   TFile* hfake = new TFile("hfake.root","recreate");
+   hfake->cd();
+   for(Int_t i=0;i<ptnumber;i++){
+	   c1[i]->cd();
+	   h1[i]->Write();
+	   h1[i]->Draw("hist");
+	   c1[i]->Print(Form("./pt%0.f_%0.f.pdf",lowpt[i],highpt[i]));
+   }
+   hfake->Close();
+}
